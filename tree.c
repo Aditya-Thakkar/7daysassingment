e590eaf0c8e793905c770d6629d4015b8e84bc61
Aditya Thakkar <98096793+Aditya-Thakkar@users.noreply.github.com>
2022-03-05 22:18:02 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -1,183 +1,113 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<unordered_map>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-using namespace std;
-
-//Data Structure to store a binary tree node;
 struct Node
 {
-    int data;
-    Node *l,*r;
-    Node(int data)
-    {
-        this->data = data;
-        this->l = NULL;
-        this->r = NULL;
-    }
-};
+	int data;
+	struct Node *left, *right;
+} * tr[100];
 
-//function to print all nodes of a given binary tree in a specific
-//order from top to bottom
+int level = 0;
+int qfront = 0;
+int qrear = 0;
 
-void printNode(Node* root)
+int line[100];
+int lindex = 0;
+int output[100][100];
+int outputlevel = 0;
+int outputcolumn = 0;
+
+void playWithArray(int level)
 {
-    //return if the tree is empty
-    if(root==NULL){
-    return;
-    }
-    
-    //print the root Node
-    cout<<root->data<<" ";
-    
-    //create two empty queues and enqueue root's left and right child, respectively
-    queue<Node*>q1,q2;
-    
-    if(root->l && root->r)
-    {
-        q1.push(root->l);
-        q2.push(root->r);
-    }
-    //loop till queue is empty
-    while(!q1.empty())
-    {
-        //calculate the total number of nodes at the current level
-        int n = q1.size();
-        
-        //process every node of the current level
-        while(n--)
-        {
-            //dequeue front node from the first queue and print it
-            Node* x = q1.front();
-            q1.pop();
-            cout<<x->data<<" ";
-            
-            //enqueue left and right child of 'x' to the first queue
-            if(x->l)
-            {
-                q1.push(x->l);
-            }
-            
-            if(x->r)
-            {
-                q1.push(x->r);
-            }
-            
-            //dequeue front node from the second queue and print it
-            Node* y = q2.front();
-            q2.pop();
-            cout<<y->data<<" ";
-            
-            //enqueue left and right child of 'x' to the first queue
-            if(y->r)
-            {
-                q2.push(y->r);
-            }
-            
-            if(y->l)
-            {
-                q2.push(y->l);
-            }
-            
-        }
-    }
+	if (level == 0)
+	{
+		output[level][outputcolumn] = 1;
+		return;
+	}
+	for (int i = 0, j = pow(2,level) - 1; i < pow(2,level) / 2, j >= pow(2,level) / 2; i++, j--)
+	{
+		output[level][outputcolumn++] = line[i];
+		output[level][outputcolumn++] = line[j];
+	}
 }
 
-//a specific order from bottom to top
-
-void priNodes(Node* root)
+void printLevelOrder(struct Node *root)
 {
-  //return if the tree is empty
-  if(root ==NULL)
-  {
-   return ;
-   }
- 
-  //start with level 1
-  int level = 1;
+	
+	if (root == NULL)
+		return;
 
-  //create an empty multimap of int associated with multiple values
-  unordered_map<int, vector<int>> map;
+	myQ[qrear++] = root;
 
-  //insert the root node at the first level
-  map[level].push_back(root->data);
-  queue<Node*>q1,q2;
-  
-  if(root->l && root->r)
-  {
-    q1.push(root->l);
-    q2.push(root->r);
-   }
+	while (q_front != q_rear)
+	{
 
-    //loop till queue is empty
-    while(!q1.empty())
-    {
-      level++;
-      int n = q1.size();
+		int nodeCount = qrear - qfront;
+		for (int i = 0; i < nodeCount; i++)
+		{
+			struct Node *node = myQ[qfront];
+			line[lindex++] = node->data;
+			qfront++;
+			if (node->left != NULL)
+				myQ[qrear++] = node->left;
 
-       while(n--)
-        {
-            //dequeue front node from the first queue and print it
-            Node* x = q1.front();
-            q1.pop();
-       
-            map[level].push_back(x->data);
-            //enqueue left and right child of 'x' to the first queue
-             if(x->l)
-            {
-                q1.push(x->l);
-            }
-            
-            if(x->r)
-            {
-                q1.push(x->r);
-            }
-            //dequeue front node from the first queue and print it
-            Node* y = q2.front();
-            q2.pop();
-            map[level].push_back(y->data);
-            //enqueue left and right child of 'x' to the first queue
-            if(y->r)
-            {
-                q2.push(y->r);
-            }
-            
-            if(y->l)
-            {
-                q2.push(y->l);
-            }
-         }
-     }
-       //print all nodes present at every level
-        for(int i=map.size();i>0; i--)
-        {
-           for(int j: map[i]){
-             cout<<j<<" ";
-        }
-     }
- }           
+			if (node->right != NULL)
+				myQ[qrear++] = node->right;
+		}
+
+		printf("\n");
+		playWithArray(level);
+		level++;
+		outputcolumn = 0;
+		lindex = 0;
+	}
+}
+
+
+struct Node *newNode(int data)
+{
+
+	struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+	temp->data = data;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+
 int main()
 {
-    Node* root = new Node(1);
-    root->l = new Node(2);
-    root->r = new Node(3);
-    root->l->l = new Node(4);
-    root->l->r = new Node(5);
-    root->r->l = new Node(6);
-    root->r->r = new Node(7);
-    root->l->l->l = new Node(8);
-    root->l->l->r = new Node(9);
-    root->l->r->l = new Node(10);
-    root->l->r->r = new Node(11);
-    root->r->l->l = new Node(12);
-    root->r->l->r = new Node(13);
-    root->r->r->l = new Node(14);
-    root->r->r->r = new Node(15);
-    cout<<"Print Top-Down"<<endl;
-    printNode(root);
-    cout<<"\nPrint Bottom-Up"<<endl;
-    priNodes(root);
-    cout<<endl;
-    return 0;
+	
+	struct Node *root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	root->right->left = newNode(6);
+	root->right->right = newNode(7);
+	root->left->left->left = newNode(8);
+	root->left->left->right = newNode(9);
+	root->left->right->left = newNode(10);
+	root->left->right->right = newNode(11);
+	root->right->left->left = newNode(12);
+	root->right->left->right = newNode(13);
+	root->right->right->left = newNode(14);
+	root->right->right->right = newNode(15);
+	
+	printLevelOrder(root);
+
+	for (int i = 0; i < level; i ++){
+		for (int j = 0; j < pow(2,i); j ++){
+			printf("%d, ", output[i][j]);
+		}
+	}
+	printf("\n");
+	for (int i = level -1 ; i >= 0; i--)
+	{
+		for (int j = 0; j < pow(2, i); j++)
+		{
+			printf("%d, ", output[i][j]);
+		}
+	}
+	return 0;
 }
